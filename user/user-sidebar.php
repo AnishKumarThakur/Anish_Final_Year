aac1bf29e7d01ad30fccf95211a7c90a70804579
Anish Kumar Thakur <49469109+AnishKumarThakur@users.noreply.github.com>
2020-09-30 09:51:49 +0545

    user added

---
+++ user/user-sidebar.php
@@ -0,0 +1,36 @@
+<div class="sidebar" data-color="azure" data-image="assets/img/sidebar-2.jpg">
+
+     <!--   you can change the color of the sidebar using: data-color="blue | azure | green | orange | red | purple" -->
+
+
+    <div class="sidebar-wrapper">
+        <div class="logo">
+            <a href="#" class="simple-text">
+                Hosanna Furnitures
+            </a>
+        </div>
+
+        <ul class="nav">
+            <li>
+                <a href="user.php">
+                    <p>User Profile</p>
+                </a>
+            </li>
+            <li>
+                <a href="change-password.php">
+                    <p>Change Password</p>
+                </a>
+            </li>
+            <li>
+                <a href="view-order.php">
+                    <p>View Orders</p>
+                </a>
+            </li>
+            <li>
+                <a href="forum.php">
+                    <p>forum</p>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
